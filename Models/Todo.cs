10d3058595f56ba789712b54cc7be4503edf9038
Orlando Fernández <orlando_fg@hotmail.com>
2021-04-16 18:21:07 +0200

    conectar a SQLDB en Azure

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -6,9 +6,11 @@ namespace DotNetCoreSqlDb.Models
     public class Todo
     {
         public int ID { get; set; }
+
+        [Display(Name = "Descripción")]
         public string Description { get; set; }
 
-        [Display(Name = "Created Date")]
+        [Display(Name = "Fecha creación")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CreatedDate { get; set; }
